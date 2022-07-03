4061625f10b0292834f6f60b6f2d8854b81ee8d1
victory316 <csh153@gmail.com>
2022-07-03 14:55:18 +0900

    템플릿 메서드 추가

---
+++ app/src/main/java/com/example/algorithmstudy/pattern/behavoiral/templatemethod/Americano.kt
@@ -0,0 +1,5 @@
+package com.example.algorithmstudy.pattern.behavoiral.templatemethod
+
+class Americano : Drink() {
+    override fun addIngredient() = println("Adding 2 shots of espresso")
+}
---
+++ app/src/test/java/com/example/algorithmstudy/design_pattern/TemplateMethodTest.kt
@@ -0,0 +1,22 @@
+package com.example.algorithmstudy.design_pattern
+
+import com.example.algorithmstudy.pattern.behavoiral.templatemethod.Americano
+import com.example.algorithmstudy.pattern.behavoiral.templatemethod.OrangeAde
+import org.junit.Test
+
+/**
+ *  TemplateMethod 패턴
+ *
+ *  - 공통된 function들은 함께 사용하되, 각 구현 요소별로 다르게 수행할 function을 각각 정의해 사용 가능한 패턴
+ */
+class TemplateMethodTest {
+
+    @Test
+    fun testTemplateMethod() {
+        val americano = Americano()
+        val orangeAde = OrangeAde()
+
+        americano.prepareDrink()
+        orangeAde.prepareDrink()
+    }
+}
