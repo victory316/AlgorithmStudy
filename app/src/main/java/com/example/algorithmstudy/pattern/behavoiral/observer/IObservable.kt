01322c364d2e8a366884fbe02c095522f6402c25
victory316 <csh153@gmail.com>
2022-07-03 15:33:18 +0900

    Observer Pattern 추가

---
+++ app/src/main/java/com/example/algorithmstudy/pattern/behavoiral/observer/IObservable.kt
@@ -0,0 +1,17 @@
+package com.example.algorithmstudy.pattern.behavoiral.observer
+
+interface IObservable {
+    val observers: ArrayList<IObserver>
+
+    fun add(observer: IObserver) {
+        observers.add(observer)
+    }
+
+    fun remove(observer: IObserver) {
+        observers.remove(observer)
+    }
+
+    fun sendUpdateEvent() {
+        observers.forEach { it.update() }
+    }
+}
