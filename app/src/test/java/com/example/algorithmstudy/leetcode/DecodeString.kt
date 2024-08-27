03ceffc9d63985f9d2db80b38d588f21a1026686
SH Choi <csh153@gmail.com>
2024-08-27 18:45:04 +0900

    Decode string 작업중

---
+++ app/src/test/java/com/example/algorithmstudy/leetcode/DecodeString.kt
@@ -0,0 +1,22 @@
+package com.example.algorithmstudy.leetcode
+
+import org.junit.Test
+
+class DecodeString {
+    fun decodeString(s: String): String {
+
+
+        return ""
+    }
+
+    tailrec fun findFormatted(s: String): String {
+        return ""
+    }
+
+    @Test
+    fun TestDecodeString() {
+        val input = "3[a]2[bc]"
+
+        assert(input == "aaabcbc")
+    }
+}
