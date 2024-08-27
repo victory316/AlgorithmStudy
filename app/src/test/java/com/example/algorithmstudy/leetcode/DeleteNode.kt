3573522b20a20bf3caf25557b8dd4d7f9c1eabe6
SH Choi <csh153@gmail.com>
2024-08-27 18:21:02 +0900

    node deletion 추가중

--- app/src/test/java/com/example/algorithmstudy/leetcode/DeleteNode.kt
+++ app/src/test/java/com/example/algorithmstudy/leetcode/DeleteNode.kt
@@ -1,18 +1,31 @@
 package com.example.algorithmstudy.leetcode
 
 class DeleteNode {
+
+    enum class LastDirection {
+        LEFT,
+        RIGHT
+    }
+
     fun deleteNode(root: TreeNode?, key: Int): TreeNode? {
         var indexNode: TreeNode? = root
         var prevNode: TreeNode? = null
+        var lastDirection : LastDirection = LastDirection.LEFT
 
         while (true) {
-            if (indexNode?.`val` == key) {
-
+            if (indexNode.`val` == key) {
+                if (lastDirection == LastDirection.LEFT) {
+                    prevNode.left
+                }
             }
-            if ((indexNode?.`val` ?: 0) < key) {
+            if (indexNode.`val` < key) {
                 prevNode = indexNode
+                indexNode = indexNode.right
+                lastDirection = LastDirection.RIGHT
             } else {
                 prevNode = indexNode
+                indexNode = indexNode.left
+                lastDirection = LastDirection.LEFT
             }
         }
     }
