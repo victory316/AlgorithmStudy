17d310eed970bbe0b7f9f5648103c51cab757ce9
Seung Hwan Choi <csh153@gmail.com>
2024-08-28 21:58:28 +0900

    MaxVowels 추가

---
+++ app/src/test/java/com/example/algorithmstudy/leetcode/MaxVowels.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun maxVowels(s: String, k: Int): Int {
+        var value = 0
+        for(i in 0 until k){
+            value += valueOf(s[i])
+        }
+        var maxValue = value
+        for(i in k until s.length){
+            value = value - valueOf(s[i - k]) + valueOf(s[i])
+            maxValue = Math.max(value, maxValue)
+        }
+        return maxValue
+    }
+
+    inline fun valueOf(c: Char): Int{
+        return if(isVowel(c)) 1 else 0
+    }
+
+    inline fun isVowel(c: Char): Boolean {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+    }
+}
