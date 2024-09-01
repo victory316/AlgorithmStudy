387fb4f2a16c156bfda6db912ec5f22954be01ed
Seung Hwan Choi <csh153@gmail.com>
2024-09-01 11:51:13 +0900

    Can place flowers 풀이

--- app/src/test/java/com/example/algorithmstudy/leetcode/CanPlaceFlowers.kt
+++ app/src/test/java/com/example/algorithmstudy/leetcode/CanPlaceFlowers.kt
@@ -1,14 +1,23 @@
 package com.example.algorithmstudy.leetcode
 
 class CanPlaceFlowers {
-
     fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
         var flowerRemains = n
 
         flowerbed.forEachIndexed { index, i ->
-            
+            val prev = if (index == 0) 0 else flowerbed[index - 1]
+            val next = if (index == flowerbed.lastIndex) 0 else flowerbed[index + 1]
+
+            if (prev == 0 && next == 0 && flowerbed[index] == 0) {
+                if (flowerRemains == 0) {
+                    return@forEachIndexed
+                } else {
+                    flowerbed[index] = 1
+                    flowerRemains--
+                }
+            }
         }
 
-        return false
+        return flowerRemains == 0
     }
 }
